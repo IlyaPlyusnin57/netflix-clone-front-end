a0412c5029bd1fa466b550441361891257ee6f40
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-31 20:43:44 -0400

    Delete commented code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-// for (let i = 0; i < document.body.childNodes.length; i++) {
-//   console.log(document.body.childNodes[i]);
-// }
-/*
-console.log(document.body.children); // HTML Collection
-
-console.log(document.body.childNodes); // Node list
-
-let link = document.querySelector("a");
-
-console.log(link.getAttribute("href"));
-*/
-
-let accWrapper = document.querySelector(".acc-wrapper");
+const accWrapper = document.querySelector(".acc-wrapper");
 
 accWrapper.addEventListener("click", (e) => {
   if (e.target.classList.contains("accordion")) {
